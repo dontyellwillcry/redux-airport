203d5807d85b26cd9a4cf90e836d5ee92e819d8b
LazyDotExe <papaporo@proton.me>
2023-08-15 15:51:57 -0500

    added npm/redux and imported

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
+
 /** TODO: import REDUX **/
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import logger from 'redux-logger'
 
 
 /** TODO: Add REDUCERS */
